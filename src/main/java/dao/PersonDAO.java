5163204a384d092d7a25f32cefb4397bdff34005
Igor Chernikov <69716334+cherrrnikov@users.noreply.github.com>
2024-04-12 23:45:55 +0300

    Update PersonDAO.java

--- src/main/java/dao/PersonDAO.java
+++ src/main/java/dao/PersonDAO.java
@@ -4,5 +4,5 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class PersonDAO {
-    // Здесь будут лежать специфические запросы к БД (с помощью SQL)
+    // Т.к. запросы примитивные, всю работу выполняет Hibernate
 }
