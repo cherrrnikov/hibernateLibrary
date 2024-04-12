8fc4e9809bd5b6fe32514ced782ba498f032936a
Igor Chernikov <69716334+cherrrnikov@users.noreply.github.com>
2024-04-12 23:43:44 +0300

    Update SpringConfig.java

--- src/main/java/config/SpringConfig.java
+++ src/main/java/config/SpringConfig.java
@@ -93,7 +93,7 @@ public class SpringConfig implements WebMvcConfigurer {
     public LocalContainerEntityManagerFactoryBean entityManagerFactory() {
         final LocalContainerEntityManagerFactoryBean em = new LocalContainerEntityManagerFactoryBean();
         em.setDataSource(dataSource());
-        em.setPackagesToScan("ru.alishev.springcourse.models");
+        em.setPackagesToScan("models");
 
         final HibernateJpaVendorAdapter vendorAdapter = new HibernateJpaVendorAdapter();
         em.setJpaVendorAdapter(vendorAdapter);
